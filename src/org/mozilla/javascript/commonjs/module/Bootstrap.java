588cc35e0b14011d3021fe63c81b138a065d675c
Chris Mowforth <chris@mowforth.com>
2012-06-26 19:05:42 +0100

    load bootstrap script in rhino

--- src/org/mozilla/javascript/commonjs/module/Bootstrap.java
+++ src/org/mozilla/javascript/commonjs/module/Bootstrap.java
@@ -1,5 +1,10 @@
 package org.mozilla.javascript.commonjs.module;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Script;
 import org.mozilla.javascript.Scriptable;
@@ -9,7 +14,14 @@ public class Bootstrap {
     private String script;
 
     public Bootstrap(String script) {
-        this.script = script;
+        String scriptPath = script;
+        Path p = Paths.get(scriptPath);
+        try {
+            byte[] bytes = Files.readAllBytes(p);
+            this.script = new String(bytes);
+        } catch (IOException e) {
+            System.err.println("Unable to load bootstrap script.");
+        }
     }
 
     public Script getScript(Context cx) {
